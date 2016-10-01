62abb5ddd66710abbfbeb83de6a006ad4de2122e
WizardAdeptus <wizard.adeptus@ya.ru>
2016-10-01 23:59:48 +0400

    Testing sockets

--- src/info/introToJava/socket/Client.java
+++ src/info/introToJava/socket/Client.java
@@ -1,6 +1,9 @@
 package info.introToJava.socket;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -11,7 +14,7 @@ public class Client {
             System.exit(-1);
         }
 
-        Socket client;
+        Socket client = null;
         try {
             // Open a client socket connection
             client = new Socket("127.0.0.1", 3000);
@@ -22,6 +25,27 @@ public class Client {
             e.printStackTrace();
         }
 
+        if (client != null) {
+            try (
+                OutputStream outputStream = client.getOutputStream();
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(client.getInputStream()))
+            ) {
+                // Send stock symbol to the server
+                outputStream.write((args[0] + "\n").getBytes());
 
+                String quote;
+                while (true) {
+                    quote = reader.readLine();
+
+                    if (quote.length() == 0) continue;
+                    if ("End".equals(quote)) break;
+
+                    System.out.println("Got the quote for " + args[0] + ":" + quote);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
